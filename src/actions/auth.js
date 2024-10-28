359af32f0b97d9272bd498fd2cbd4b0c90524240
Abdullah Alhumaidhan <abdullahkh56@gmail.com>
2024-10-28 09:25:38 +0300

    Initial commit

---
+++ src/actions/auth.js
@@ -0,0 +1,50 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { deleteToken, setToken } from "@/lib/token";
+import { baseUrl, getHeaders } from "./config";
+import { redirect } from "next/navigation";
+
+export async function login(formData) {
+  const userData = Object.fromEntries(formData);
+
+  const response = await fetch(`${baseUrl}/auth/login`, {
+    method: "POST",
+    headers: await getHeaders(),
+    body: JSON.stringify(userData),
+  });
+
+  // Extract and store the token
+  const { token } = await response.json();
+  await setToken(token);
+  // Redirect to the `/notes` page
+  redirect("/notes");
+}
+
+export async function register(formData) {
+  const response = await fetch(`${baseUrl}/auth/register`, {
+    method: "POST",
+    body: formData,
+  });
+
+  // Extract and store the token
+  const { token } = await response.json();
+  await setToken(token);
+
+  revalidatePath("/users");
+  // Redirect to the `/notes` page
+  redirect("/notes");
+}
+
+export async function logout() {
+  // What do you need to do to logout?
+  await deleteToken();
+  // Where should you redirect the user?
+  redirect("/");
+}
+
+export async function getAllUsers() {
+  const response = await fetch(`${baseUrl}/auth/users`);
+  const users = response.json();
+  return users;
+}
---
+++ src/actions/config.js
@@ -0,0 +1,15 @@
+import { getToken } from "@/lib/token";
+
+const baseUrl = "https://react-bank-project.eapi.joincoded.com/";
+
+async function getHeaders() {
+  const headers = new Headers();
+  const { token } = await getToken();
+  headers.append("Content-Type", "application/json");
+  // Add an authorization header
+  headers.append("Authorization", `Bearer ${token}`);
+
+  return headers;
+}
+
+export { getHeaders, baseUrl };
